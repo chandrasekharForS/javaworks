35f1898e66011402da14d89731d072e120f005db
Chandrasekhar <chandru.p.b.cms@gmail.com>
2026-06-30 19:50:09 +0530

    Thread001.java file is modified

--- threads-project-001/src/com/threads/demo/Thread001.java
+++ threads-project-001/src/com/threads/demo/Thread001.java
@@ -1,10 +1,25 @@
 package com.threads.demo;
 
+class MyThread extends Thread{
+	public void run() {
+		System.out.println("Thread: " + Thread.currentThread().getName() + 
+				" is running.");
+	}
+}
 public class Thread001 {
 
 	public static void main(String[] args) {
-		System.out.println("Testing....");
-
+		MyThread t1 = new MyThread();
+		t1.setName("First");
+		MyThread t2 = new MyThread();
+		t2.setName("Second");
+		MyThread t3 = new MyThread();
+		t3.setName("Third");
+		
+		t1.start();
+		t2.start();
+		t3.start();
+		
 	}
 
 }
